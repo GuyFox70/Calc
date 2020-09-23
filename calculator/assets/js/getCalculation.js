3c897acc3b4ecfc0b378883293efabd054c7d2a8
Orlov Artem Sergeevich <artm.orlov.88@list.ru>
2020-09-23 12:54:43 +0500

    feat: overright js file

--- calculator/assets/js/getCalculation.js
+++ calculator/assets/js/getCalculation.js
@@ -16,15 +16,29 @@
     if (target.className == 'calc__keyboard') return;
 
     let action = (target.getAttribute('data-sing') || target.parentElement.getAttribute('data-sing')).trim();
-    // if (input.value == undefined) input.value = '';
 
-    num1.innerHTML += action;
+    if (action == 'ac') {
+      addText(num1, '');
+      addStyle([num2, result], 'hide');
+    } else {
+      addText(num1, action, true);
+    }
   }
 
   function createElement(elem, parent, text = undefined, style = undefined) {
     const tag = document.createElement(elem);
     if (style) tag.classList.add(style);
     parent.appendChild(tag);
+  }
+
+  function addText(elem, text, flag) {
+    flag ? elem.innerHTML += text : elem.innerHTML = text;
+  }
+
+  function addStyle(arr, style) {
+    arr.forEach(elem => {
+      elem.classList.add(style);
+    })
   }
 
 })();
